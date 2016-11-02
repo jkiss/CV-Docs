ff90f9bbbc2366f25a93ce8d1879890bcd7f2439
- 木马人 - <itjkiss@gmail.com>
2016-11-02 11:56:45 +0800

    Update wx-login.js

--- 2016/wx-web-auth/wx-login.js
+++ 2016/wx-web-auth/wx-login.js
@@ -17,23 +17,23 @@ function parseUrlParams(){
 }
 
 var UrlParams = parseUrlParams(),
-	appid = 'wx88888888888888';
+    appid = 'wx88888888888888';
 
 if(!!UrlParams.code){
-	$.ajax({
-		type: "post",
-		url: '',
-		dataType: "json",
-		data: {
-			code: UrlParams.code,
-		},
-		success: function(data){
-			console.log(data);
-		},
-		fail: function(e){}
-	});
+  $.ajax({
+    type: "post",
+    url: '',
+    dataType: "json",
+    data: {
+      code: UrlParams.code,
+    },
+    success: function(data){
+      console.log(data);
+    },
+    fail: function(e){}
+  });
 }else{
-	window.location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='+appid+'&redirect_uri='+location.href.split("#")[0]+'&response_type=code&scope=snsapi_userinfo&state=2016#wechat_redirect';
+  window.location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='+appid+'&redirect_uri='+location.href.split("#")[0]+'&response_type=code&scope=snsapi_userinfo&state=2016#wechat_redirect';
 }
 
 
